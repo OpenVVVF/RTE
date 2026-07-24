125e13716a0d43ccb189e0921457f9d11e96d825
Thomas Liao <taliao@ucsc.edu>
2026-07-23 17:41:21 -0700

    Use distinct telemetry keys for codegen current sink
    Change ph_u_a/ph_v_a/ph_w_a to cg_iu_a/cg_iv_a/cg_iw_a so the codegen
    path is distinguishable from the base image's CurrentSensorTest_RunOnce()
    telemetry.

--- Assets/NodeTemplates/app.telemetry_current_sink/inline.cpp
+++ Assets/NodeTemplates/app.telemetry_current_sink/inline.cpp
@@ -1,3 +1,3 @@
-platform_telemetry_log_f32("ph_u_a", i_abc.a.in(au::amperes));
-platform_telemetry_log_f32("ph_v_a", i_abc.b.in(au::amperes));
-platform_telemetry_log_f32("ph_w_a", i_abc.c.in(au::amperes));
+platform_telemetry_log_f32("cg_iu_a", i_abc.a.in(au::amperes));
+platform_telemetry_log_f32("cg_iv_a", i_abc.b.in(au::amperes));
+platform_telemetry_log_f32("cg_iw_a", i_abc.c.in(au::amperes));
