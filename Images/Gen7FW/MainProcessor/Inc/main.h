bdab4629e053a9625f30284a6ad598fa4990f18e
Thomas Liao <taliao@ucsc.edu>
2026-08-05 15:59:42 -0700

    Start Gen7 Firmware

---
+++ Images/Gen7FW/MainProcessor/Inc/main.h
@@ -0,0 +1,217 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define FRAM_SPI_SCK_Pin GPIO_PIN_2
+#define FRAM_SPI_SCK_GPIO_Port GPIOE
+#define DEBUG_GREEN_LED_Pin GPIO_PIN_3
+#define DEBUG_GREEN_LED_GPIO_Port GPIOE
+#define FRAM_SPI_MISO_Pin GPIO_PIN_5
+#define FRAM_SPI_MISO_GPIO_Port GPIOE
+#define FRAM_SPI_MOSI_Pin GPIO_PIN_6
+#define FRAM_SPI_MOSI_GPIO_Port GPIOE
+#define FRAM_WP_Pin GPIO_PIN_13
+#define FRAM_WP_GPIO_Port GPIOC
+#define FRAM_CS_Pin GPIO_PIN_14
+#define FRAM_CS_GPIO_Port GPIOC
+#define FRAM_HOLD_Pin GPIO_PIN_15
+#define FRAM_HOLD_GPIO_Port GPIOC
+#define ONBOARD_TEMP_SENSE_I2C_SDA_Pin GPIO_PIN_0
+#define ONBOARD_TEMP_SENSE_I2C_SDA_GPIO_Port GPIOF
+#define ONBOARD_TEMP_SENSE_I2C_SCL_Pin GPIO_PIN_1
+#define ONBOARD_TEMP_SENSE_I2C_SCL_GPIO_Port GPIOF
+#define PERIPHERAL_POWER_ENABLE_Pin GPIO_PIN_2
+#define PERIPHERAL_POWER_ENABLE_GPIO_Port GPIOF
+#define AIN_MOTOR_TMP_Pin GPIO_PIN_4
+#define AIN_MOTOR_TMP_GPIO_Port GPIOF
+#define AIN_THROTTLE_B_Pin GPIO_PIN_6
+#define AIN_THROTTLE_B_GPIO_Port GPIOF
+#define AIN_TMP_SENSE_1_Pin GPIO_PIN_8
+#define AIN_TMP_SENSE_1_GPIO_Port GPIOF
+#define AIN_ENCODER_SIN_HALL_U_Pin GPIO_PIN_0
+#define AIN_ENCODER_SIN_HALL_U_GPIO_Port GPIOC
+#define AIN_ENCODER_COS_HALL_V_Pin GPIO_PIN_1
+#define AIN_ENCODER_COS_HALL_V_GPIO_Port GPIOC
+#define AIN_PH_W_CURSENS_REF_Pin GPIO_PIN_2
+#define AIN_PH_W_CURSENS_REF_GPIO_Port GPIOC
+#define AIN_PH_W_CURSENS_Pin GPIO_PIN_3
+#define AIN_PH_W_CURSENS_GPIO_Port GPIOC
+#define AIN_TMP_SENSE_3_Pin GPIO_PIN_0
+#define AIN_TMP_SENSE_3_GPIO_Port GPIOA
+#define AIN_TMP_SENSE_2_Pin GPIO_PIN_1
+#define AIN_TMP_SENSE_2_GPIO_Port GPIOA
+#define AIN_HALL_W_Pin GPIO_PIN_2
+#define AIN_HALL_W_GPIO_Port GPIOA
+#define AIN_THROTTLE_A_Pin GPIO_PIN_3
+#define AIN_THROTTLE_A_GPIO_Port GPIOA
+#define PHASE_CURRENT_COMPARIATOR_N_Pin GPIO_PIN_4
+#define PHASE_CURRENT_COMPARIATOR_N_GPIO_Port GPIOA
+#define DC_LINK_CURRENT_COMPARITOR_N_Pin GPIO_PIN_5
+#define DC_LINK_CURRENT_COMPARITOR_N_GPIO_Port GPIOA
+#define AIN_PH_V_CURSENS_Pin GPIO_PIN_6
+#define AIN_PH_V_CURSENS_GPIO_Port GPIOA
+#define AIN_PH_V_CURSENS_REF_Pin GPIO_PIN_7
+#define AIN_PH_V_CURSENS_REF_GPIO_Port GPIOA
+#define AIN_PH_U_CURSENS_Pin GPIO_PIN_4
+#define AIN_PH_U_CURSENS_GPIO_Port GPIOC
+#define AIN_PH_U_CURSENS_REF_Pin GPIO_PIN_5
+#define AIN_PH_U_CURSENS_REF_GPIO_Port GPIOC
+#define AIN_PH_W_CURSENS_REFB0_Pin GPIO_PIN_0
+#define AIN_PH_W_CURSENS_REFB0_GPIO_Port GPIOB
+#define AIN_PH_W_CURSENSB1_Pin GPIO_PIN_1
+#define AIN_PH_W_CURSENSB1_GPIO_Port GPIOB
+#define AIN_DC_LINK_CURSENS_Pin GPIO_PIN_11
+#define AIN_DC_LINK_CURSENS_GPIO_Port GPIOF
+#define AIN_DC_LINK_CURSENS_REF_Pin GPIO_PIN_12
+#define AIN_DC_LINK_CURSENS_REF_GPIO_Port GPIOF
+#define AIN_PH_W_CURSENSF13_Pin GPIO_PIN_13
+#define AIN_PH_W_CURSENSF13_GPIO_Port GPIOF
+#define AIN_PH_W_CURSENS_REFF14_Pin GPIO_PIN_14
+#define AIN_PH_W_CURSENS_REFF14_GPIO_Port GPIOF
+#define VOLTAGE_MONITOR_I2C_SDA_Pin GPIO_PIN_15
+#define VOLTAGE_MONITOR_I2C_SDA_GPIO_Port GPIOF
+#define MULTIDIRVE_FIBER_SYNC_LINE_Pin GPIO_PIN_7
+#define MULTIDIRVE_FIBER_SYNC_LINE_GPIO_Port GPIOE
+#define PH_U_HIGH_Pin GPIO_PIN_8
+#define PH_U_HIGH_GPIO_Port GPIOE
+#define PH_U_LOW_Pin GPIO_PIN_9
+#define PH_U_LOW_GPIO_Port GPIOE
+#define PH_V_HIGH_Pin GPIO_PIN_10
+#define PH_V_HIGH_GPIO_Port GPIOE
+#define PH_V_LOW_Pin GPIO_PIN_11
+#define PH_V_LOW_GPIO_Port GPIOE
+#define PH_W_HIGH_Pin GPIO_PIN_12
+#define PH_W_HIGH_GPIO_Port GPIOE
+#define PH_W_LOW_Pin GPIO_PIN_13
+#define PH_W_LOW_GPIO_Port GPIOE
+#define GATE_DRIVER_FAULT_PWM_BREAK_Pin GPIO_PIN_15
+#define GATE_DRIVER_FAULT_PWM_BREAK_GPIO_Port GPIOE
+#define COPROCESSOR_SYNC_Pin GPIO_PIN_8
+#define COPROCESSOR_SYNC_GPIO_Port GPIOD
+#define COPROCESSOR_WAKEUP_Pin GPIO_PIN_9
+#define COPROCESSOR_WAKEUP_GPIO_Port GPIOD
+#define COPROCESSOR_RESET_Pin GPIO_PIN_10
+#define COPROCESSOR_RESET_GPIO_Port GPIOD
+#define COPROCESSOR_BOOTSEL_Pin GPIO_PIN_11
+#define COPROCESSOR_BOOTSEL_GPIO_Port GPIOD
+#define VOLTAGE_MONITOR_I2C_SCL_Pin GPIO_PIN_12
+#define VOLTAGE_MONITOR_I2C_SCL_GPIO_Port GPIOD
+#define USER_DIN_6_Pin GPIO_PIN_15
+#define USER_DIN_6_GPIO_Port GPIOD
+#define USER_DIN_5_Pin GPIO_PIN_2
+#define USER_DIN_5_GPIO_Port GPIOG
+#define USER_DIN_4_Pin GPIO_PIN_3
+#define USER_DIN_4_GPIO_Port GPIOG
+#define USER_DIN_3_Pin GPIO_PIN_4
+#define USER_DIN_3_GPIO_Port GPIOG
+#define USER_DIN_2_Pin GPIO_PIN_5
+#define USER_DIN_2_GPIO_Port GPIOG
+#define USER_DIN_1_Pin GPIO_PIN_6
+#define USER_DIN_1_GPIO_Port GPIOG
+#define USER_DIN_7_Pin GPIO_PIN_7
+#define USER_DIN_7_GPIO_Port GPIOG
+#define USER_DIN_8_Pin GPIO_PIN_8
+#define USER_DIN_8_GPIO_Port GPIOG
+#define COPROCESSOR_TIMER_SYNC_OUTPUT_Pin GPIO_PIN_6
+#define COPROCESSOR_TIMER_SYNC_OUTPUT_GPIO_Port GPIOC
+#define CANBUS_POWER_ENABLE_Pin GPIO_PIN_8
+#define CANBUS_POWER_ENABLE_GPIO_Port GPIOC
+#define GATE_DRIVER_POWER_ENABLE_Pin GPIO_PIN_10
+#define GATE_DRIVER_POWER_ENABLE_GPIO_Port GPIOC
+#define GATE_DRIVER_FAULT_Pin GPIO_PIN_11
+#define GATE_DRIVER_FAULT_GPIO_Port GPIOC
+#define GATE_DRIVER_READY_Pin GPIO_PIN_12
+#define GATE_DRIVER_READY_GPIO_Port GPIOC
+#define MULTIDRIVE_UART_RX_FIBER_Pin GPIO_PIN_0
+#define MULTIDRIVE_UART_RX_FIBER_GPIO_Port GPIOD
+#define VSENSE_ISO_ADC_INTERRUPT_Pin GPIO_PIN_1
+#define VSENSE_ISO_ADC_INTERRUPT_GPIO_Port GPIOD
+#define SPI2_CS_Pin GPIO_PIN_2
+#define SPI2_CS_GPIO_Port GPIOD
+#define GATE_DRIVE_PWR2_FEEDBACK_Pin GPIO_PIN_3
+#define GATE_DRIVE_PWR2_FEEDBACK_GPIO_Port GPIOD
+#define GATE_DRIVE_PWR1_FEEDBACK_Pin GPIO_PIN_4
+#define GATE_DRIVE_PWR1_FEEDBACK_GPIO_Port GPIOD
+#define GATE_DRIVER_RESET_Pin GPIO_PIN_5
+#define GATE_DRIVER_RESET_GPIO_Port GPIOD
+#define POWERMON_SLEEP_Pin GPIO_PIN_6
+#define POWERMON_SLEEP_GPIO_Port GPIOD
+#define POWERMON_ENABLE_Pin GPIO_PIN_7
+#define POWERMON_ENABLE_GPIO_Port GPIOD
+#define USER_DOUT_4_Pin GPIO_PIN_11
+#define USER_DOUT_4_GPIO_Port GPIOG
+#define USER_DOUT_3_Pin GPIO_PIN_12
+#define USER_DOUT_3_GPIO_Port GPIOG
+#define USER_DOUT_2_Pin GPIO_PIN_13
+#define USER_DOUT_2_GPIO_Port GPIOG
+#define USER_DOUT_1_Pin GPIO_PIN_14
+#define USER_DOUT_1_GPIO_Port GPIOG
+#define MULTIDRIVE_UART_TX_FIBER_Pin GPIO_PIN_9
+#define MULTIDRIVE_UART_TX_FIBER_GPIO_Port GPIOB
+#define DEBUG_ORANGE_LED_Pin GPIO_PIN_1
+#define DEBUG_ORANGE_LED_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Images/Gen7FW/MainProcessor/Src/usart.c
@@ -0,0 +1,371 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart4;
+UART_HandleTypeDef huart9;
+UART_HandleTypeDef huart3;
+DMA_HandleTypeDef hdma_usart3_tx;
+
+/* UART4 init function */
+void MX_UART4_Init(void)
+{
+
+  /* USER CODE BEGIN UART4_Init 0 */
+
+  /* USER CODE END UART4_Init 0 */
+
+  /* USER CODE BEGIN UART4_Init 1 */
+
+  /* USER CODE END UART4_Init 1 */
+  huart4.Instance = UART4;
+  huart4.Init.BaudRate = 115200;
+  huart4.Init.WordLength = UART_WORDLENGTH_8B;
+  huart4.Init.StopBits = UART_STOPBITS_1;
+  huart4.Init.Parity = UART_PARITY_NONE;
+  huart4.Init.Mode = UART_MODE_TX_RX;
+  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart4.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart4.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart4, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart4, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART4_Init 2 */
+
+  /* USER CODE END UART4_Init 2 */
+
+}
+/* UART9 init function */
+void MX_UART9_Init(void)
+{
+
+  /* USER CODE BEGIN UART9_Init 0 */
+
+  /* USER CODE END UART9_Init 0 */
+
+  /* USER CODE BEGIN UART9_Init 1 */
+
+  /* USER CODE END UART9_Init 1 */
+  huart9.Instance = UART9;
+  huart9.Init.BaudRate = 115200;
+  huart9.Init.WordLength = UART_WORDLENGTH_8B;
+  huart9.Init.StopBits = UART_STOPBITS_1;
+  huart9.Init.Parity = UART_PARITY_NONE;
+  huart9.Init.Mode = UART_MODE_TX_RX;
+  huart9.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart9.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart9.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart9.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart9.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart9) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart9, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart9, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart9) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART9_Init 2 */
+
+  /* USER CODE END UART9_Init 2 */
+
+}
+/* USART3 init function */
+
+void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 460800;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart3.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart3, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart3, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  if(uartHandle->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspInit 0 */
+
+  /* USER CODE END UART4_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_UART4;
+    PeriphClkInitStruct.Usart234578ClockSelection = RCC_USART234578CLKSOURCE_D2PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* UART4 clock enable */
+    __HAL_RCC_UART4_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**UART4 GPIO Configuration
+    PD0     ------> UART4_RX
+    PB9     ------> UART4_TX
+    */
+    GPIO_InitStruct.Pin = MULTIDRIVE_UART_RX_FIBER_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(MULTIDRIVE_UART_RX_FIBER_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = MULTIDRIVE_UART_TX_FIBER_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(MULTIDRIVE_UART_TX_FIBER_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UART4_MspInit 1 */
+
+  /* USER CODE END UART4_MspInit 1 */
+  }
+  else if(uartHandle->Instance==UART9)
+  {
+  /* USER CODE BEGIN UART9_MspInit 0 */
+
+  /* USER CODE END UART9_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_UART9;
+    PeriphClkInitStruct.Usart16ClockSelection = RCC_USART16910CLKSOURCE_D2PCLK2;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* UART9 clock enable */
+    __HAL_RCC_UART9_CLK_ENABLE();
+
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    /**UART9 GPIO Configuration
+    PG0     ------> UART9_RX
+    PG1     ------> UART9_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF11_UART9;
+    HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UART9_MspInit 1 */
+
+  /* USER CODE END UART9_MspInit 1 */
+  }
+  else if(uartHandle->Instance==USART3)
+  {
+  /* USER CODE BEGIN USART3_MspInit 0 */
+
+  /* USER CODE END USART3_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3;
+    PeriphClkInitStruct.Usart234578ClockSelection = RCC_USART234578CLKSOURCE_D2PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* USART3 clock enable */
+    __HAL_RCC_USART3_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**USART3 GPIO Configuration
+    PB10     ------> USART3_TX
+    PB11     ------> USART3_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* USART3 DMA Init */
+    /* USART3_TX Init */
+    hdma_usart3_tx.Instance = DMA1_Stream0;
+    hdma_usart3_tx.Init.Request = DMA_REQUEST_USART3_TX;
+    hdma_usart3_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart3_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart3_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart3_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart3_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart3_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart3_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart3_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_usart3_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(uartHandle,hdmatx,hdma_usart3_tx);
+
+    /* USART3 interrupt Init */
+    HAL_NVIC_SetPriority(USART3_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART3_IRQn);
+  /* USER CODE BEGIN USART3_MspInit 1 */
+
+  /* USER CODE END USART3_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspDeInit 0 */
+
+  /* USER CODE END UART4_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART4_CLK_DISABLE();
+
+    /**UART4 GPIO Configuration
+    PD0     ------> UART4_RX
+    PB9     ------> UART4_TX
+    */
+    HAL_GPIO_DeInit(MULTIDRIVE_UART_RX_FIBER_GPIO_Port, MULTIDRIVE_UART_RX_FIBER_Pin);
+
+    HAL_GPIO_DeInit(MULTIDRIVE_UART_TX_FIBER_GPIO_Port, MULTIDRIVE_UART_TX_FIBER_Pin);
+
+  /* USER CODE BEGIN UART4_MspDeInit 1 */
+
+  /* USER CODE END UART4_MspDeInit 1 */
+  }
+  else if(uartHandle->Instance==UART9)
+  {
+  /* USER CODE BEGIN UART9_MspDeInit 0 */
+
+  /* USER CODE END UART9_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART9_CLK_DISABLE();
+
+    /**UART9 GPIO Configuration
+    PG0     ------> UART9_RX
+    PG1     ------> UART9_TX
+    */
+    HAL_GPIO_DeInit(GPIOG, GPIO_PIN_0|GPIO_PIN_1);
+
+  /* USER CODE BEGIN UART9_MspDeInit 1 */
+
+  /* USER CODE END UART9_MspDeInit 1 */
+  }
+  else if(uartHandle->Instance==USART3)
+  {
+  /* USER CODE BEGIN USART3_MspDeInit 0 */
+
+  /* USER CODE END USART3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART3_CLK_DISABLE();
+
+    /**USART3 GPIO Configuration
+    PB10     ------> USART3_TX
+    PB11     ------> USART3_RX
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
+
+    /* USART3 DMA DeInit */
+    HAL_DMA_DeInit(uartHandle->hdmatx);
+
+    /* USART3 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USART3_IRQn);
+  /* USER CODE BEGIN USART3_MspDeInit 1 */
+
+  /* USER CODE END USART3_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
